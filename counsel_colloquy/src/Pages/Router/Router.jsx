b516a59a97564305665f3e878f104c996f506e16
Ebaad <ebaadazam82881@gmail.com>
2024-05-05 13:50:00 +0530

    Initial Commit

---
+++ counsel_colloquy/src/Pages/Router/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Homepage from '../HomePage/Homepage'
+
+const Router = () => {
+  return (
+    <div>
+        <div className="flex">
+            <div>
+<Sidebar/>
+            </div>
+            <div>
+<Routes>
+    <Route path='/' element={<Homepage/>}></Route>
+</Routes>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Router
